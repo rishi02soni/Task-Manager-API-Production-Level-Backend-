f6fd238c3b37aca008bf6cd27360b49f7bd4a2ca
Rishi Soni <167216843+rishi02soni@users.noreply.github.com>
2026-03-24 21:35:04 +0530

    Create Task.js

---
+++ models/Task.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+  title: String,
+  completed: { type: Boolean, default: false },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Task", taskSchema);
