a92d0bbfd800bebe746c0e089c7e026d3e4d93c0
Rishi Soni <167216843+rishi02soni@users.noreply.github.com>
2026-03-24 21:35:57 +0530

    Create userController.js

---
+++ controllers/userController.js
@@ -0,0 +1,40 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// Register
+exports.register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if (user) return res.status(400).json({ msg: "User exists" });
+
+    const hashed = await bcrypt.hash(password, 10);
+
+    user = new User({ name, email, password: hashed });
+    await user.save();
+
+    res.json({ msg: "User Registered" });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+// Login
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ msg: "Invalid creds" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid creds" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.json({ token });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
